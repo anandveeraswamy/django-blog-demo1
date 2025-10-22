76e5daf773494142d80754b8144843edfd612cbf
Anand <anandveeraswamy@gmail.com>
2025-10-22 21:52:41 +0100

    changed urls

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,8 +130,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_URL = '/media/'
-MEDIA_URL = '/opt/render/project/src/media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
